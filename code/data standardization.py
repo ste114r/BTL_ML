18bc9b5a115b279565fadd9f0c50e99722fba8e3
ste114r <uehi.1270@gmail.com>
2025-01-08 09:28:13 +0700

    Cleaned and changed

--- code/data standardization.py
+++ code/data standardization.py
@@ -2,7 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 # Read the data from the CSV file
-file_path = 'C:/Users/Admin/Documents/0TL/ml/BTL/data/train 100.csv'
+file_path = 'C:/Users/Admin/Documents/0TL/ml/BTL/data/test.csv'
 data = pd.read_csv(file_path)
 
 # Identify continuous numerical columns (e.g., float64)
@@ -13,7 +13,7 @@ scaler = StandardScaler()
 data[continuous_columns] = scaler.fit_transform(data[continuous_columns])
 
 # Save the standardized data to a new file
-standardized_file_path = 'C:/Users/Admin/Documents/0TL/ml/BTL/data/train 100 standardized.csv'
+standardized_file_path = 'C:/Users/Admin/Documents/0TL/ml/BTL/data/test_standardized.csv'
 data.to_csv(standardized_file_path, index=False)
 
 # Display the first few rows of the standardized data
